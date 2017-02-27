3094b9899991298c8b89c5d9e8fe5a26fec5e339
jtroberg <johanna.troberg@helsinki.fi>
2017-02-27 13:23:05 +0200

    Creating data wrangling script

---
+++ data_wrangling.R
@@ -0,0 +1,19 @@
+# Data wrangling
+
+# Johanna Troberg
+# johanna.troberg@helsinki.fi
+# 27.2.2017
+
+# The data used in this final assignment is combination of my previously published data about dog 
+# and human enzymes, as well as other published articles from the same study object.
+
+# List of references given at the end of the final assignment web page.
+
+# The data was first collected into one excel file. 
+
+#Data wrangling should be done in a separate R-script and included in your repository and a link 
+#to that script should be included in you web page. If you are using a data set already processed
+# during the course, the script should have clearly commented section(s) which highlight new
+# changes you have made to the data. Only these will grant you points for data wrangling.
+# The data wrangling script should also include your full name, email, date and a short file
+# description and the code should be clearly commented.
